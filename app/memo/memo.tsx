fee80907e365efc0cdb58fab34a41367d574be70
Mischa <mischa0619@gmail.com>
2025-06-19 14:57:38 +0800

    refactor: useMemo - 整理 todo 檔案

--- app/memo/memo.tsx
+++ app/memo/memo.tsx
@@ -1,33 +1,11 @@
-import { useState } from "react";
-
-import { createTodos } from "./utils.js";
-import TodoList from "./TodoList";
-
-const todos = createTodos();
+import Index from "./Todo/index";
 
 export function Memo() {
   console.log("Memo component rendered ---");
 
-  const [tab, setTab] = useState("all");
-  const [isDark, setIsDark] = useState(false);
-
   return (
     <>
-      <div className="flex gap-4">
-        <button onClick={() => setTab("all")}>All</button>
-        <button onClick={() => setTab("active")}>Active</button>
-        <button onClick={() => setTab("completed")}>Completed</button>
-      </div>
-      <label>
-        <input
-          type="checkbox"
-          checked={isDark}
-          onChange={(e) => setIsDark(e.target.checked)}
-        />
-        Dark mode
-      </label>
-      <hr />
-      <TodoList todos={todos} tab={tab} theme={isDark ? "dark" : "light"} />
+      <Index />
     </>
   );
 }
