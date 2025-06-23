490449238d607cbf5b588e84d8b9775ba5dcc0e4
Mischa <mischa0619@gmail.com>
2025-06-23 16:11:16 +0800

    feat: middlewares - devtools

--- app/store/useCounterStore.ts
+++ app/store/useCounterStore.ts
@@ -1,15 +1,23 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useCounterStore = create((set) => ({
-  count: 0,
-  increase: () => set((state) => ({ count: state.count + 1 })),
-  decrease: () => set((state) => ({ count: state.count - 1 })),
-  count2: 0,
-  increase2: () => set((state) => ({ count2: state.count2 + 1 })),
-  decrease2: () => set((state) => ({ count2: state.count2 - 1 })),
-  count3: 0,
-  increase3: () => set((state) => ({ count3: state.count3 + 1 })),
-  decrease3: () => set((state) => ({ count3: state.count3 - 1 })),
-}));
+const useCounterStore = create(
+  devtools(
+    (set) => ({
+      count: 0,
+      increase: () => set((state) => ({ count: state.count + 1 })),
+      decrease: () => set((state) => ({ count: state.count - 1 })),
+      count2: 0,
+      increase2: () => set((state) => ({ count2: state.count2 + 1 })),
+      decrease2: () => set((state) => ({ count2: state.count2 - 1 })),
+      count3: 0,
+      increase3: () => set((state) => ({ count3: state.count3 + 1 })),
+      decrease3: () => set((state) => ({ count3: state.count3 - 1 })),
+    }),
+    {
+      name: "Counter Store",
+    }
+  )
+);
 
 export default useCounterStore;
