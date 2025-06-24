9571b33fef54cf535e2ec79c5b48d04f58ea935b
Mischa <mischa0619@gmail.com>
2025-06-24 16:52:20 +0800

    feat: middlewares - combine
    merging state and function

--- app/store/useCounterStore.ts
+++ app/store/useCounterStore.ts
@@ -1,19 +1,23 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, combine } from "zustand/middleware";
 
 const useCounterStore = create(
   devtools(
-    (set) => ({
-      count: 0,
-      increase: () => set((state) => ({ count: state.count + 1 })),
-      decrease: () => set((state) => ({ count: state.count - 1 })),
-      count2: 0,
-      increase2: () => set((state) => ({ count2: state.count2 + 1 })),
-      decrease2: () => set((state) => ({ count2: state.count2 - 1 })),
-      count3: 0,
-      increase3: () => set((state) => ({ count3: state.count3 + 1 })),
-      decrease3: () => set((state) => ({ count3: state.count3 - 1 })),
-    }),
+    combine(
+      {
+        count: 0,
+        count2: 0,
+        count3: 0,
+      },
+      (set) => ({
+        increase: () => set((state) => ({ count: state.count + 1 })),
+        decrease: () => set((state) => ({ count: state.count - 1 })),
+        increase2: () => set((state) => ({ count2: state.count2 + 1 })),
+        decrease2: () => set((state) => ({ count2: state.count2 - 1 })),
+        increase3: () => set((state) => ({ count3: state.count3 + 1 })),
+        decrease3: () => set((state) => ({ count3: state.count3 - 1 })),
+      })
+    ),
     {
       name: "Counter Store",
     }
