894300f5ed108fd75c88a92a4640d840efb841d6
Mischa <mischa0619@gmail.com>
2025-06-25 11:38:12 +0800

    feat: middlewares - persist partially
    - persisting a state partially

--- app/store/useCounterPersistStore.ts
+++ app/store/useCounterPersistStore.ts
@@ -5,9 +5,15 @@ const useCounterPersistStore = create()(
   persist(
     (set) => ({
       count: 0,
+      list: [],
       increase: () => set((state) => ({ count: state.count + 1 })),
     }),
-    { name: "counter-storage" }
+    {
+      name: "counter-storage",
+      partialize: (state) => ({
+        list: state.list,
+      }),
+    }
   )
 );
 
